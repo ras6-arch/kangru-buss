713063750b03a32f6019f2310516f8456c98ad09
ras6-arch <kadre.saar@gmail.com>
2026-08-31 23:55:31 +0300

    Force PWA cache refresh for latest timetable fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'kangru-buss-v4';
+const CACHE = 'kangru-buss-v7';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
