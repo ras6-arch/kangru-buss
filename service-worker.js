b307710d9a0af5ebc32205aaba186670633e1602
ras6-arch <kadre.saar@gmail.com>
2026-08-31 22:39:31 +0300

    Force refresh cached bus app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'kangru-buss-v3';
+const CACHE = 'kangru-buss-v4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
